1c60f650d44e02b1ff11d858d70e482727581161
Denis <C:\Users\User\AppData\Roaming\The Bat!>
2019-12-27 19:55:39 +0200

    lesson 87

--- way-of-samurai-2.0/src/components/Profile/MyPosts/MyPosts.jsx
+++ way-of-samurai-2.0/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 import cls from './MyPosts.module.css'
 import Post from './Post/Post';
 import NewPost from './NewPost/NewPost';
-const MyPosts = (props) => {
+const MyPosts = React.memo((props) => {
+    console.log('RENDER');
     let myPosts = props.myPosts.map((el, idx) => {
         return <Post key={idx} message={el.text}/>
     })
+    
     return(
+        
         <div className={cls.wrapper}>
             <NewPost addNewPost={props.addNewPost}/>
             {myPosts}
         </div>
     )
-}
+})
 export default MyPosts;
--- way-of-samurai-2.0/src/components/Profile/Status/StatusWithHooks.jsx
+++ way-of-samurai-2.0/src/components/Profile/Status/StatusWithHooks.jsx
@@ -1,10 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import cls from './Status.module.css'
 
 const StatusWithHooks = (props) => {
 
 let [editMode, setEditMode] = useState(false)
 let [status, setStatus] = useState(props.status)
+useEffect(() => {
+    setStatus(props.status)
+}, [props.status])
 
 const activeMode = () => {
     setEditMode(true)
