a7a500d3b2cffa3401847a83dae7c33602e31dab
Denis <C:\Users\User\AppData\Roaming\The Bat!>
2019-12-27 22:09:49 +0200

    lesson 89

--- way-of-samurai-2.0/src/redux/auth-reducer.js
+++ way-of-samurai-2.0/src/redux/auth-reducer.js
@@ -19,35 +19,32 @@ const authReducer = (state = initialState, action) => {
             return state;
     }
 }
+
 export const setAuthUserData = (id,email,login,isAuth) => ({type:auth_SET_USER_DATA, data:{id,email,login,isAuth}})
-export const getAuthUserData = () => (dispatch) => {
-    return authAPI.me()
-        .then(responce=>{
-            if(responce.data.resultCode === 0){
-                let {id, email, login ,isAuth} = responce.data.data
-                dispatch(setAuthUserData(id, email, login ,true))
-            };
-        })
-}
-export const login = (email, password, rememberMe) => (dispatch) => {
 
-    authAPI.login(email, password, rememberMe)
-        .then(responce=>{
-            if(responce.data.resultCode === 0){
-                dispatch(getAuthUserData())
-            } else {
-                let message = responce.data.messages.length > 0 ? responce.data.messages[0] : 'Some error'
-                dispatch(stopSubmit('login', {_error:message}))
-            };
-        })
+export const getAuthUserData = () => async (dispatch) => {
+    let responce = await authAPI.me()
+        if(responce.data.resultCode === 0){
+            let {id, email, login ,isAuth} = responce.data.data
+            dispatch(setAuthUserData(id, email, login ,true))
+        };
 }
-export const logout = () => (dispatch) => {
-    authAPI.logout()
-        .then(responce=>{
-            if(responce.data.resultCode === 0){
-                dispatch(setAuthUserData(null, null, null ,false))
 
-            };
-        })
+export const login = (email, password, rememberMe) => async(dispatch) => {
+
+    let responce = await authAPI.login(email, password, rememberMe)
+        if(responce.data.resultCode === 0){
+            dispatch(getAuthUserData())
+        } else {
+            let message = responce.data.messages.length > 0 ? responce.data.messages[0] : 'Some error'
+            dispatch(stopSubmit('login', {_error:message}))
+        };
+}
+
+export const logout = () => async (dispatch) => {
+    let responce = await authAPI.logout()
+        if(responce.data.resultCode === 0){
+            dispatch(setAuthUserData(null, null, null ,false))
+        }
 }
 export default authReducer;
